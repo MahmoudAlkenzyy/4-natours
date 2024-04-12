8e8fa4860cbac65653408d8d140a04b5e6b66487
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2024-04-12 16:37:52 +0200

    handle dublicate  DB fields and validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,6 +4,19 @@ const handleCastErrorDB = (err) => {
   const message = `Inavalid ${err.path}: ${err.value}`;
   return new AppError(message, 404);
 };
+const handleDublicateFieldsDB = (err) => {
+  const value = JSON.stringify(err.keyValue);
+
+  const message = `Inavalid ${value}: please use another value `;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `inavalid input data : ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   err.statusCode = err.statusCode || 501;
   err.status = err.status || 'error';
@@ -16,17 +29,16 @@ const sendErrorDev = (err, res) => {
 };
 const sendErrorProd = (err, res) => {
   if (err.isOperation) {
-    err.statusCode = err.statusCode || 501;
-    err.status = err.status || 'error';
     res.status(err.statusCode).json({
       status: err.status,
       messsage: err.message,
     });
   } else {
     console.log('Error 💥', err);
-    res.status(501).json({
+    res.status(500).json({
       status: 'error',
       message: 'something went very wrong',
+      err,
     });
   }
 };
@@ -38,7 +50,8 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (err.name === 'CastError') error = handleCastErrorDB(error);
-    console.log(error);
+    if (err.code === 11000) error = handleDublicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
