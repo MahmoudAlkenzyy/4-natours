1c48e22e8ecdaf8ca3d415b03e04183513041b8f
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2024-04-12 22:08:03 +0200

    heandle unhandled Rejection and uncaught Exception errors

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION💥 Shetting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -25,6 +31,14 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`the server listening in ${port}....`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.message);
+  console.log('unHandled rejection 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
